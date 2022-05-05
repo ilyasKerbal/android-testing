e6a04989817c9b717c9fcaf411f63f1004798a1f
ILYAS KERBAL <kerbalsc@gmail.com>
2022-05-05 10:19:35 +0000

    Testing Coroutine Timing

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModelTest.kt
@@ -3,10 +3,13 @@ package com.example.android.architecture.blueprints.todoapp.statistics
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.android.architecture.blueprints.todoapp.MainCoroutineRule
 import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 @ExperimentalCoroutinesApi
 class StatisticsViewModelTest {
@@ -31,5 +34,19 @@ class StatisticsViewModelTest {
         tasksRepository = FakeTestRepository()
 
         statisticsViewModel = StatisticsViewModel(tasksRepository)
+    }
+
+    @Test
+    fun loadTasks_loading() {
+        mainCoroutineRule.pauseDispatcher()
+        // Load the task in the view model.
+        statisticsViewModel.refresh()
+
+        // Then progress indicator is shown.
+        assertThat(statisticsViewModel.dataLoading.getOrAwaitValue(), `is`(true))
+
+        mainCoroutineRule.resumeDispatcher()
+        // Then progress indicator is hidden.
+        assertThat(statisticsViewModel.dataLoading.getOrAwaitValue(), `is`(false))
     }
 }
